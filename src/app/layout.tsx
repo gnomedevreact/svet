55cd7d0fdb731bad80e60c36174c04bc39f3629a
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2024-05-09 20:48:23 +0200

    select-none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const ephesis = Ephesis({ weight: "400", subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Xenia Asveta",
   description: "Xenia Asveta web-page",
+
   openGraph: {
     title: "Xenia Asveta",
     description: "Xenia Asveta web-page",
