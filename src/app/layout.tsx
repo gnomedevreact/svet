cec6a3645c3be6db49e914377316f8d5bfc95e54
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2024-05-09 20:29:43 +0200

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,20 @@ import type { Metadata, Viewport } from "next";
 import "./globals.scss";
 
 import { Ephesis } from "next/font/google";
-import Head from "next/head";
 
 const ephesis = Ephesis({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Xenia Asveta",
   description: "Xenia Asveta web-page",
+  openGraph: {
+    title: "Xenia Asveta",
+    description: "Xenia Asveta web-page",
+  },
+  twitter: {
+    title: "Xenia Asveta",
+    description: "Xenia Asveta web-page",
+  },
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
